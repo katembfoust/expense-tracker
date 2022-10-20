b0c20a9b88154e51d319b02d86713e4f2e816ddc
Kate Foust <kate@adeptassistance.com>
2022-10-20 15:11:11 -0400

    expense tracker

---
+++ src/components/expenses/Expenses.js
@@ -0,0 +1,33 @@
+import ExpensesFilter from "../filter/ExpensesFilter";
+import React, { useState } from "react";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+function Expenses(props) {
+  const [selectedYear, setSelectedYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === selectedYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={selectedYear}
+          onChangeYear={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
